6948adb36f2e677143c82b9e4ceb40b0e62f8db2
festusev <evandellis@gmail.com>
2018-03-19 19:12:49 -0500

    Updated Player Class

--- Player.js
+++ Player.js
@@ -1,12 +1,29 @@
 class Player extends GameObject
 {
-  constructor(x, y, img, w, h, health, gold, weapons, armor)
+  this.defW = 10;
+  this.defH = 10;
+  this.defImg = "GameImages\player.png";
+  this.health = 100;
+  this.gold = 0;
+  this.weapons = new WeaponsPack();
+  this.armor = new ArmorPack();
+  this.coffer = 0;
+  constructor(x, y, img, gold, weapons, armor)
   {
-    super(x,y,img,w,h);
-    this.health = health;
-    this.gold = gold;
-    this.weapons = weapons;
-    this.armor = armor;
+    /*
+      Here I check to see if all of the parameters of the constructor
+      have been met, if not then I keep the default values as they
+      are.
+    */
+    img = typeof(img)==undefined ? this.defImg : img;
+    this.gold = typeof(gold)==undefined ? this.gold : gold;
+    this.weapons = typeof(weapons)==undefined ? this.weapons : weapons;
+    this.armor = typeof(armor)==undefined ? this.armor : armor;
+    super(x,y,img, this.defW,this.defH);
+  }
+  incHealth(num)
+  {
+    this.health+=num;
   }
   setHealth(h)
   {
@@ -23,5 +40,13 @@ class Player extends GameObject
   setArmor(a)
   {
     this.armor=a;
+  }
+  setCoffers(c)
+  {
+    this.coffer=c;
+  }
+  incCoffers(num)
+  {
+    this.coffer +=num;
   }
 }
