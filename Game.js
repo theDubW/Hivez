4e5283c8345bf24ff13c4becd2f7ce4f36dffa0a
theDubW <36181957+theDubW@users.noreply.github.com>
2018-03-19 21:30:31 -0500

    Update Game.js

--- Game.js
+++ Game.js
@@ -1,11 +1,16 @@
 public class Game
 {
-  constructor(team, room){
+  constructor(team, room, player){
     this.team = team;
     this.room = room;
+    this.player = player;
   }
   run(){
-
+  	move();
+  	draw();
+  }
+  move(){
+  	player.checkBoundaries(); //This is an imaginary function that should be in the player class to check if its inside the boundaries
   }
   draw(){
     
