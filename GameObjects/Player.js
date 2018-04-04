88f83a5c97af3de2a555355ecc33ecc3f8ec852c
festusev <evandellis@gmail.com>
2018-04-03 21:27:57 -0500

    More changes to player

--- GameObjects/Player.js
+++ GameObjects/Player.js
@@ -3,14 +3,7 @@ module.exports = class Player extends GameObject
 {
   constructor(playerId, team, x, y, width, height, name, img, gold)
   {
-<<<<<<< HEAD
     super(x,y,img||"./public/images/Bart.gif", width||10, height||10);
-=======
-    super(x,y,img || this.defImg, width, height);
-    this.defW = 10;
-    this.defH = 10;
-    this.defImg = './images/Bart.gif';
->>>>>>> 2d78d98d63a9b89ce33cb7efc300bb672cea3993
     this.health = 100;
     this.gold = gold || 0;
     this.pressingRight = false;
@@ -23,7 +16,7 @@ module.exports = class Player extends GameObject
     this.playerId = playerId;
     this.name=name;
     this.orientation = 0;
-      }
+  }
   incHealth(num)
   {
     this.health+=num;
@@ -40,10 +33,6 @@ module.exports = class Player extends GameObject
     }
     if(this.pressingDown == true){
       this.y += this.playerYSpeed;
-<<<<<<< HEAD
-
-=======
->>>>>>> 2d78d98d63a9b89ce33cb7efc300bb672cea3993
     }
   }
   incGold(g)
