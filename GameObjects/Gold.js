5afa04f408fea3cbbdda3faa5b496298ffcba683
festusev <evandellis@gmail.com>
2018-04-08 14:47:08 -0500

    Fixed Gold.js

--- GameObjects/Gold.js
+++ GameObjects/Gold.js
@@ -1,8 +1,7 @@
 const gameObject = require("./GameObject.js");
 module.exports = class Gold extends GameObject
 {
-  this.defImg = "./public/images/gold.jpg";
   constructor(x, y, w, h){
-    super(x,y,defImg, w,h);
+    super(x,y,"./public/images/gold.jpg", w,h);
   }
 }
